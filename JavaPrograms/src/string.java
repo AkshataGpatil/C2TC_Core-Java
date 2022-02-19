82aa4f2a515ed709a4ec634e62d5d19322cbe57e
dell <dell@DESKTOP-U4BUB5F>
2022-02-19 14:51:53 +0530

    First commit

---
+++ JavaPrograms/src/string.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+
+public class string {
+
+	public static void main(String[] args) {
+		String input="Computer";
+		StringBuilder input1=new StringBuilder();
+		input1.append(input);
+		input1.reverse();
+		System.out.println(input1);
+
+	}
+
+}
